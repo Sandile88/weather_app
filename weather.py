c41d7936ffb8aa3931750cc172b92ea5361129b2
Sandile88 <sandilep.mremi@gmail.com>
2024-03-18 06:27:14 +0200

    Implemented flask and refactored code for easier readability

--- weather.py
+++ weather.py
@@ -1,105 +1,79 @@
 import requests
 import json
 import os
+import pycountry
 from dotenv import load_dotenv
 from dataclasses import dataclass
-# from flask import Flask, render_template, request
+from icon_symbols import *
 
 
-# app = Flask(__name__)
 load_dotenv()
+api_key = os.environ['API_KEY']    
 
 @dataclass
 class WeatherData:
-    city_name: str
-    country: str
+
     main: str
     description: str
     icon: str
     temperature: float
     speed: float
 
+    def __str__(self) -> str:
+        output = '\n'.join([f" {key}: {value}°C" if 'temp' or 'feel_like'  in key  else f"   {key}: {value}" for key, value in self.temperature.items() if key in ['temp', 'feels_like', 'temp_min', 'temp_max']])
+        symbol = icon_symbols.get(self.icon, self.icon)
+        return f"\nCurrent weather:\n\nMain: {self.main}, {self.description}\n Icon: {symbol}\nTemperatures: \n{output}\nPressure: {self.temperature['pressure']}hPa\nHumidity: {self.temperature['humidity']}% \nSpeed: {self.speed} m/s"
 
-# @app.route('/')
-# def home():
-#     return render_template('data.html')
-
-
-# @app.route(methods = ['POST'])
-def get_weather_data(city):
-    api_key = os.environ['API_KEY']    
-    # city = request.form['information']
-    # url= f'https://api.openweathermap.org/data/2.5/weather?q={city},{country_code}&appid={api_key}&units=metric'
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+def get_weather_data(city, country):
+    url= f'https://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={api_key}&units=metric'
     
     fetch = requests.get(url)
     response = fetch.json()
-    
+    with open ('info.json', 'w') as f:
+        json.dump(response, f, indent=2)    
+
     if 'message' in response and response['message'] == 'city not found':
+            print('City not found. Please enter a valid city name.')
+    elif not pycountry.countries.get(name=country.capitalize()):
+        print('Country not found. Please enter a valid country code.')
         return None
     else:
-        return response
 
-# @app.route('get_weather_information')
-def main():
-    try:
-        while True:
-            city = input('Enter City: ')
-            # country_code = input('Enter Country: ')
-
-            response = get_weather_data(city)
-
-
-            if response:
-                break
-            else:
-                print('City not found. Please enter a valid city name.')
-
+        temp_data = response['main']
+        temp_keys =['temp', 'feels_like', 'temp_min', 'temp_max']
+        for key in temp_keys:
+            if key in temp_data:
+                temp_data[key] = int(temp_data[key])
         data = WeatherData(
-            city_name = response.get('name'),
-            country = response.get('sys').get('country'),
             main = response['weather'][0]['main'],
             description = response['weather'][0]['description'],
             icon = response['weather'][0]['icon'],
-            temperature = response['main'],
+            temperature = temp_data,
             speed = response['wind'].get('speed')
         )
 
-        with open ('data.json', 'w') as f:
-            json.dump(response, f, indent=2)
         return data
 
-        # print(city_name, country, weather, weather_description, weather_icon, temperature, wind)
 
-        # print('\nWeather report: ')
-        # print(f'Area: {city_name}, {country}')
-        # print(f'Weather: {weather}')
-        # print(f'Description: {weather_description}')
-        # print('Temperature: ')
-        # for key, value in temps.items():
-        #     if key != 'pressure' and key != 'humidity':
-        #         print(f'{" " * 12}{key}: {value - 273.15:.1f}°C')
-        #     if key == 'pressure':
-        #         print(f'{" " * 12}{key}: {value}hPa')
-        #     if key == 'humidity':
-        #         print(f'{" " * 12}{key}: {value}%')
+def main():
+    try:
+        while True:
+            city = input('City?: ').lower()
+            country = input('Country?: ').capitalize()
 
+            response = get_weather_data(city, country)
 
+            if response:
+                print(response)
+                break
 
-        # print('Wind:')
-        # for key, value in wind.items():
-        #     if key == 'speed':
-        #         print(f'{" " * 5}{key}: {value}m/s')
-        #     if key == 'deg':
-        #         print(f'{" " * 5}{key}: {value}°')
+        response_dict = response.__dict__
+        with open ('data.json', 'w') as f:
+            json.dump(response_dict, f, indent=2)
 
-    
-        # with open ('data.json', 'w') as f:
-        #     json.dump(response, f, indent=2)
     except Exception as e:
         exit(f'An error occured: \n{e}')
 
-    # return render_template('data.html', information=data)
 
 if __name__ == '__main__':
-    print(main())
+    main()
