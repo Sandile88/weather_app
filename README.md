7bf3dc004de8afd695e4b47b91eaf8f349fb4aa0
WeThinkCode_ <wethinkcode_@pop-os.localdomain>
2024-04-23 12:18:40 +0200

    implemted error handling for none data retrieval, changing of the background depending for every city search

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# weather_app
+## Flask Weather App
+- This is a simple Flask web application that allows users to search for weather information based on city and country inputs. 
+- It retrieves weather data from the OpenWeatherMap API and displays it to the user.
+
+## Features
+
+1. Users can input a city and country to get weather information.
+2. Weather information includes main weather conditions, description, temperature, wind speed, pressure, and humidity.
+3. Background images are dynamically fetched from the Pexels API based on the searched city.
+4. If no suitable image is found for a city, a default background image is displayed.
+
+
+## Installation
+1. Clone the repository:
+
+git clone https://github.com/Sandile88/flask-weather-app.git
+
+2. Navigate to the project directory:
+
+cd flask-weather-app
+
+
+3. Install dependencies using pip:
+pip install -r requirements.txt
+
+4. Set up your API keys:
+
+- Obtain an API key from OpenWeatherMap and Pexels.
+- Create a .env file in the project root directory.
+- Add your API keys to the .env file:
+
+    API_KEY=your_openweathermap_api_key
+    PEXELS_API_KEY=your_pexels_api_key
+
+
+## Usage
+Run the Flask application:
+    1. python app.py
+    2. Open a web browser and go to http://localhost:5000 to access the application.
+    3. Enter a city and country in the input fields and click the "Find" button to get weather information.
+
+
+## Credits
+This application uses data from the OpenWeatherMap API and images from the Pexels API.
+
+Licence:
+MIT
+
+
+
+
+
+
--- app.py
+++ app.py
@@ -1,19 +1,65 @@
 from flask import Flask, render_template, request
 from weather import *
+import requests
+from dotenv import load_dotenv
+from requests.exceptions import ConnectionError
+import os
 
+load_dotenv()
 app = Flask(__name__)
+PEXELS_API_KEY = os.environ['PEXELS_API_KEY']
+
+DEFAULT_BACKGROUND_IMAGE = 'https://images.pexels.com/photos/2189696/pexels-photo-2189696.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+
+def get_background_images(city):
+    url = f'https://api.pexels.com/v1/search?query={city}&per_page=1'
+    headers = {'Authorization': PEXELS_API_KEY}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    if data['total_results'] > 0:
+        return data['photos'][0]['src']['original']
+
+
+    return DEFAULT_BACKGROUND_IMAGE
+   
+
+get_background_images('South Africa')
 
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
-    data = None
+    try:  
+        data = ""
+        error_message = None
+        background_image = None
 
-    if request.method == 'POST':
-        city = request.form['cityName']
-        country = request.form['countryName']
-        data = get_weather_data(city, country)
-    return render_template('data.html', data=data)
+        if request.method == 'POST':
+            city = request.form['cityName'].lower()
+            country = request.form['countryName'].lower()
+            data = get_weather_data(city, country)
+            background_image = get_background_images(city)
+
+
+            if not city:
+                error_message =f"City '{city}' not found. Please try again."
+                return render_template('data.html',error_message=error_message, background_image=background_image)
+            if not country:
+                error_message =f"City '{country}' not found. Please try again."
+                return render_template('data.html',error_message=error_message, background_image=background_image)
+            if data is None:
+                error_message = f"City '{city}' and country '{country}' not found. Please try again."
+                return render_template('data.html',error_message=error_message, background_image=background_image)
+
+
+        return render_template('data.html', data=data, error_message=error_message, background_image=background_image)
+    except ConnectionError:
+        exit('An internet connection error occured. Please try again.')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+
+
+
+
