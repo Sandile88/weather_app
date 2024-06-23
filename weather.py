4b0b314bff6dd97bc8aefd902104d8554b110eca
Sandile88 <sandilep.mremi@gmail.com>
2024-06-23 15:49:07 +0200

    retrived 7 days worth of weather data

--- weather.py
+++ weather.py
@@ -14,6 +14,7 @@ api_key = os.environ['API_KEY']
 @dataclass
 class WeatherData:
 
+    timestamp: str
     main: str
     description: str
     icon: str
@@ -23,16 +24,41 @@ class WeatherData:
     def __str__(self) -> str:
         output = '\n'.join([f" {key}: {value}°C" if 'temp' or 'feel_like'  in key  else f"   {key}: {value}" for key, value in self.temperature.items() if key in ['temp', 'feels_like', 'temp_min', 'temp_max']])
         symbol = icon_symbols.get(self.icon, self.icon)
-        return f"\nCurrent weather:\n\nMain: {self.main}, {self.description}\n Icon: {symbol}\nTemperatures: \n{output}\nPressure: {self.temperature['pressure']}hPa\nHumidity: {self.temperature['humidity']}% \nSpeed: {self.speed} m/s"
+        return f"""\n
+        Current weather:\n\n
+        Date: {self.timestamp}\n
+        Main: {self.main}, {self.description}\n 
+        Icon: {symbol}\n
+        Temperatures: \n{output}\n
+        Pressure: {self.temperature['pressure']}hPa\n
+        Humidity: {self.temperature['humidity']}% \n
+        Speed: {self.speed} m/s
+        """
 
 
 def get_weather_data(city, country):
-    url= f'https://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={api_key}&units=metric'
+    url= f'https://api.openweathermap.org/data/2.5/forecast?q={city},{country}&include=daily&appid={api_key}&units=metric'
+    # url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={api_key}'
     
     fetch = requests.get(url)
     response = fetch.json()
+
+    # lon = response['coord']['lon']
+    # lat = response['coord']['lat']
+    # print(lon, lat)
     with open ('info.json', 'w') as f:
         json.dump(response, f, indent=2)    
+
+    # exclude = 'minutely, hourly'
+    # url2 = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={exclude}&appid={api_key}'
+
+    # fetch2 = requests.get(url2)
+    # response2 = fetch2.json()
+
+    # with open ('info2.json', 'w') as f:
+    #     json.dump(response2, f, indent=2)
+
+    
 
     if 'message' in response and response['message'] == 'city not found':
             print('City not found. Please enter a valid city name.')
@@ -40,23 +66,25 @@ def get_weather_data(city, country):
         print('Country not found. Please enter a valid country code.')
         return None
     else:
+        weather_data_list = []
+        for forecast in response['list']:
 
-        temp_data = response['main']
-        temp_keys =['temp', 'feels_like', 'temp_min', 'temp_max']
-        for key in temp_keys:
-            if key in temp_data:
-                temp_data[key] = int(temp_data[key])
-        data = WeatherData(
-            main = response['weather'][0]['main'],
-            description = response['weather'][0]['description'],
-            icon = response['weather'][0]['icon'],
-            temperature = temp_data,
-            speed = response['wind'].get('speed')
-        )
+            temp_data = forecast['main']
+            temp_keys =['temp', 'feels_like', 'temp_min', 'temp_max']
+            for key in temp_keys:
+                if key in temp_data:
+                    temp_data[key] = int(temp_data[key])
+            data = WeatherData(
+                timestamp=forecast['dt_txt'],
+                main = forecast['weather'][0]['main'],
+                description = forecast['weather'][0]['description'],
+                icon = forecast['weather'][0]['icon'],
+                temperature = temp_data,
+                speed = forecast['wind'].get('speed')
+            )
+            weather_data_list.append(data)
 
-        return data
-
-
+        return weather_data_list
 def main():
     try:
         while True:
@@ -66,10 +94,11 @@ def main():
             response = get_weather_data(city, country)
 
             if response:
-                print(response)
+                for weather in response:
+                    print(weather)
                 break
 
-        response_dict = response.__dict__
+        response_dict = [weather.__dict__ for weather in response]
         with open ('data.json', 'w') as f:
             json.dump(response_dict, f, indent=2)
 
